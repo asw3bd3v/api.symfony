778d227a9913fe6625a34def9ff7ac02c7e41c86
azernov <azernov@incom-sr.ru>
2024-05-14 16:42:16 +0300

    Урок 9: тестирование подписки - пишем тесты для SubscribeController

--- docs/lesson_5.md
+++ docs/lesson_5.md
@@ -20,7 +20,14 @@ php bin/phpunit .\tests\Repository\BookRepositoryTest.php
 composer require --dev helmich/phpunit-json-assert
 ```
 
-Важно: на текущий момент пакет не совместим с PHPUnit 11. Поэтому в файле E:\OSPanel6\home\api.symfony\vendor\helmich\phpunit-json-assert\src\Constraint\JsonValueMatchesSchema.php изменен мною конструктор.
+Важно: на текущий момент пакет не совместим с PHPUnit 11.
+Поэтому в файлах:
+
+-   vendor\helmich\phpunit-json-assert\src\Constraint\JsonValueMatchesSchema.php
+-   vendor\helmich\phpunit-json-assert\src\Constraint\JsonValueMatches.php
+-   vendor\helmich\phpunit-json-assert\src\Constraint\JsonValueMatchesMany.php
+
+изменен мною конструктор.
 
 ```php
 public function __construct($schema)
---
+++ tests/Controller/SubscribeControllerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Controller;
+
+use App\Tests\AbstractControllerTest;
+use Symfony\Component\HttpFoundation\Response;
+
+class SubscribeControllerTest extends AbstractControllerTest
+{
+    public function testSubscribe(): void
+    {
+        $content = json_encode(['email' => 'test@test.com', 'agreed' => true]);
+        $this->client->request('POST', '/api/v1/subscribe', [], [], [], $content);
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function testSubscribeNotAgreed(): void
+    {
+        $content = json_encode(['email' => 'test@test.com']);
+        $this->client->request('POST', '/api/v1/subscribe', [], [], [], $content);
+        $responseContent = json_decode($this->client->getResponse()->getContent());
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_BAD_REQUEST);
+        $this->assertJsonDocumentMatches($responseContent, [
+            '$.message' => 'Validation failed',
+            '$.details.violations' => self::countOf(1),
+            '$.details.violations[0].field' => 'agreed',
+        ]);
+    }
+}
