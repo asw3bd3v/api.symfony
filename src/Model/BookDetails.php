d33ed4d201524da86c1e7a25a484170e1fb0e5de
azernov <azernov@incom-sr.ru>
2024-05-15 16:06:51 +0300

    Урок 10: страница книги - создаем новые модели - меняем BookCategoryListItem на BookCategory

---
+++ src/Model/BookDetails.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace App\Model;
+
+class BookDetails
+{
+    private int $id;
+    private string $title;
+    private string $slug;
+    private string $image;
+    private array $authors;
+    private bool $meap;
+    private int $publicationDate;
+    private float $rating;
+    private int $reviews;
+
+    /**
+     * @var BookCategory[]
+     */
+    private array $categories;
+
+    /**
+     * @var BookFormat[]
+     */
+    private array $formats;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getSlug(): string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): static
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getImage(): string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): static
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getAuthors(): array
+    {
+        return $this->authors;
+    }
+
+    /**
+     * @param string[] $authors
+     */
+    public function setAuthors(array $authors): static
+    {
+        $this->authors = $authors;
+
+        return $this;
+    }
+
+    public function isMeap(): bool
+    {
+        return $this->meap;
+    }
+
+    public function setMeap(bool $meap): static
+    {
+        $this->meap = $meap;
+
+        return $this;
+    }
+
+    public function getPublicationDate(): int
+    {
+        return $this->publicationDate;
+    }
+
+    public function setPublicationDate(int $publicationDate): static
+    {
+        $this->publicationDate = $publicationDate;
+
+        return $this;
+    }
+
+    public function getRating(): float
+    {
+        return $this->rating;
+    }
+
+    public function setRating(float $rating): static
+    {
+        $this->rating = $rating;
+
+        return $this;
+    }
+
+    public function getReviews(): int
+    {
+        return $this->reviews;
+    }
+
+    public function setReviews(int $reviews): static
+    {
+        $this->reviews = $reviews;
+
+        return $this;
+    }
+}
