2e33f5de65a57a070aff448f20378642ca9732a5
azernov <azernov@incom-sr.ru>
2024-05-20 15:24:02 +0300

    Урок 11: тестирование страницы книги - пишем новые тесты для BookController

--- tests/Controller/BookControllerTest.php
+++ tests/Controller/BookControllerTest.php
@@ -4,6 +4,8 @@ namespace App\Tests\Controller;
 
 use App\Entity\Book;
 use App\Entity\BookCategory;
+use App\Entity\BookFormat;
+use App\Entity\BookToBookFormat;
 use App\Tests\AbstractControllerTest;
 
 class BookControllerTest extends AbstractControllerTest
@@ -45,6 +47,50 @@ class BookControllerTest extends AbstractControllerTest
         ]);
     }
 
+    public function testBookById(): void
+    {
+        $bookId = $this->createBook();
+
+        $this->client->request('GET', '/api/v1/book/' . $bookId);
+        $responseContent = json_decode($this->client->getResponse()->getContent(), true);
+
+        $this->assertResponseIsSuccessful();
+        $this->assertJsonDocumentMatchesSchema($responseContent, [
+            'type' => 'object',
+            'required' => [
+                'id', 'title', 'slug', 'image', 'authors', 'meap', 'publicationDate', 'rating', 'reviews', 'categories', 'formats',
+            ],
+            'properties' => [
+                'id' => ['type' => 'integer'],
+                'title' => ['type' => 'string'],
+                'slug' => ['type' => 'string'],
+                'image' => ['type' => 'string'],
+                'meap' => ['type' => 'boolean'],
+                'publicationDate' => ['type' => 'integer'],
+                'authors' => [
+                    'type' => 'array',
+                    'items' => [
+                        'type' => 'string',
+                    ]
+                ],
+                'rating' => ['type' => 'number'],
+                'reviews' => ['type' => 'integer'],
+                'categories' => [
+                    'type' => 'array',
+                    'items' => [
+                        'type' => 'object',
+                        'required' => ['id', 'title', 'slug'],
+                        'properties' => [
+                            'id' => ['type' => 'integer'],
+                            'title' => ['type' => 'string'],
+                            'slug' => ['type' => 'string'],
+                        ],
+                    ]
+                ],
+            ]
+        ]);
+    }
+
     private function createCategory(): int
     {
         $bookCategory = (new BookCategory())->setTitle('Devices')->setSlug('devices');
@@ -66,5 +112,42 @@ class BookControllerTest extends AbstractControllerTest
         $this->entityManager->flush();
 
         return $bookCategory->getId();
+    }
+
+    private function createBook(): int
+    {
+        $bookCategory = (new BookCategory())->setTitle('Devices')->setSlug('devices');
+        $this->entityManager->persist($bookCategory);
+
+        $format = (new BookFormat())
+            ->setTitle('format')
+            ->setDescription('description format')
+            ->setComment(null);
+
+        $this->entityManager->persist($format);
+
+        $book = (new Book())
+            ->setTitle("Test book")
+            ->setSlug("test-book")
+            ->setImage("http://localhost.png")
+            ->setMeap(true)
+            ->setIsbn('123321')
+            ->setDescription('test description')
+            ->setPublicationDate(new \DateTimeImmutable(""))
+            ->setAuthors(['Tester'])
+            ->addCategory($bookCategory);
+
+        $this->entityManager->persist($book);
+
+        $join = (new BookToBookFormat())->setPrice(123.55)
+            ->setFormat($format)
+            ->setDiscountPercent(5)
+            ->setBook($book);
+
+        $this->entityManager->persist($join);
+
+        $this->entityManager->flush();
+
+        return $book->getId();
     }
 }
